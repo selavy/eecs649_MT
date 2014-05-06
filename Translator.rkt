ed05414d72090b0e756da30aaeacd54565ab1cb2
Peter Lesslie <pclesslie@gmail.com>
2014-05-06 08:47:56 -0500

    Going to school

--- Translator.rkt
+++ Translator.rkt
@@ -12,22 +12,25 @@
 ; the sentence?  Right now I am using (propery (property (part-of-speech word))), etc. But
 ; is there a better way?
 
-(define nouns '((block blocks) (cat cats) (dog dogs)))
-(define verbs '((is are) (run runs) (eats eat))) ; get rid of is/are?
-(define articles '(the a an))
-(define adjectives '(big small black white))
-(define fr_noms '((bloc blocs) (chat chats) (chien chiens)))
-(define fr_verbes '(((court courent) (mange mangent))))
-(define fr_articles '((le la) (les les) (un une)))
-(define fr_adjectives '((grand grande) (petit) (petite) (noir noire) (blanc blanche)))
-
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;; SIMPLE SUBSTITUTION TRANSLATOR ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define DB '((block bloc) (cat chat) (dog chien) (is est) (big grand) (black noir) (white blanc) (the le) (a un) (small petit) (run court) (eat mange)))
 (define (simple-subst sent)
   (map (λ(word) (cadr (assoc word DB))) sent))
 ;(simple-subst '(the block is black))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define nouns '((block blocks) (cat cats) (dog dogs)))
+(define verbs '((is are) (run runs) (eats eat))) ; get rid of is/are?
+(define articles '(the a an))
+(define adjectives '(big small black white))
+(define prepositions '(to onto from on))
+(define fr_noms '((bloc blocs) (chat chats) (chien chiens)))
+(define fr_verbes '(((court courent) (mange mangent))))
+(define fr_articles '((le la) (les les) (un une)))
+(define fr_adjectives '((grand grande) (petit) (petite) (noir noire) (blanc blanche)))
+
+
+
 
 (define (is-noun? w)
   (ormap (λ(x)
@@ -48,10 +51,8 @@
 (define (is-article? w)
   (ormap (λ(x) (if (eq? w x) (list 'article w) #f)) articles))
 
-(define (is-adjective? w) (let ((l (member w adjectives))) (if l (list 'adjective (first l)) #f)))
-
-;(define (mark-up sent)
-;  (list (list 'noun-phrase (is-article? (first sent)) (is-noun? (cadr sent))) (list 'verb-phrase (is-verb? (caddr sent)) (is-adjective (cadddr sent)))))
+(define (is-adjective? w)   (let ([l (member w adjectives)])   (if l (list 'adjective (first l))   #f)))
+(define (is-preposition? w) (let ([l (member w prepositions)]) (if l (list 'preposition (first l)) #f)))
 
 (define (noun-and-or-adj? p)
   (let ([adj? (and (pair? p) (is-adjective? (first p)))])
@@ -64,29 +65,35 @@
   (let* ([art (is-article? (first p))] [noun (is-noun? (first p))] [adj (is-adjective? (first p))])
   (cond
     (art (and (pair? p) (let ([noun (noun-and-or-adj? (cdr p))]) (list art noun))))
+    (adj (and (pair? p) (let ([noun (noun-and-or-adj? (cdr p))]) (list adj noun))))
     (noun)
-    (adj)
     (else #f)
      )))
+
+;
+; Error in the grammar, allows multiple nouns in object phrase
+(define (is-object-phrase? p)
+  (let ([adj (is-adjective? (first p))])
+  (cond
+    (adj (and (pair? p) (let ([obj (is-object-phrase? (cdr p))]) (list adj obj))))
+    ((is-noun? p))
+    (else #f))))
+
+(define (is-verb-phrase? p)
+  (let* ([verb (is-verb? (first p))])
+  (cond
+    (verb)
+    (else #f))))
 
 (define (mark-up sent)
   (cond
     ((is-noun-phrase? sent))
+    ((is-verb-phrase? sent))
     (else #f)))
 
-;(define (mark-up sent)
-;  (cond
-;    ((is-noun? sent))
-;    ((is-verb? sent))
-;    ((is-adjective? sent))
-;    ((is-article? sent))
-;    ((is-noun? (first sent)) (printf "Is noun\n"))
-;    ((is-verb? (first sent)) (printf "Is verb\n"))
-;    ((is-adjective? (first sent)) (printf "Is adjective\n"))
-;    ((is-article? (first sent)) (printf "Is article\n"))
-;    (else #f))
-;  )
-
 ;(mark-up '(the big cat eats the small black dog))
-(trace noun-and-or-adj?)
-(mark-up '(the big black dog))
+;(trace noun-and-or-adj?)
+;(mark-up '(the big black dog))
+;(mark-up '(the dog))
+;(mark-up '(the big black small white cat))
+(is-object-phrase? '(big dog))
