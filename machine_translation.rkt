dbec96e1a0860ff791c1ded3c3f967249fab3197
Peter Lesslie <pclesslie@gmail.com>
2014-05-08 08:27:56 -0500

    Add summary at top

--- machine_translation.rkt
+++ machine_translation.rkt
@@ -1,3 +1,33 @@
+; Peter Lesslie
+; 2208496
+; EECS 649
+;
+; Basic Translator using sentence structure to approximate sentence meaning.
+;
+; Machine translation and natural language processing has proved to be one of the most difficult areas
+; of Artificial Intelligence and Computer Science as a whole.  Translation is a complex task that not
+; only involves understanding the semantic and grammar rules of a language, but also knowledge of the
+; writers intent.  Additionally, multiple words in a language may serve different functions.  For example,
+; as demonstrated in the examples "Buffalo" may refer to the city in New York, the animal (bison), or
+; the verb that means "to bully". The classic and famous example is "Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo",
+; which is a valid English sentence.
+;
+; My translator parses the sentence using the basic grammar S-V, S-V-DO, or S-V-IO (S = subject, V = verb, DO = direct object, 
+; IO = indirect object).  Once the part-of-speech for a word has been determined it is easier to choose which french word
+; or phrase to replace the english word with.
+;
+; Significant problems with this approach: 
+;     +Only handle certain sentence structures
+;     +Mine does not handle gender
+;     +Difficult to re-order words correctly
+;     +Mine only handles singular nouns
+;     +Does not combine words (contractions that in English are optional, that are not in French) such as "à + le" = "au", etc
+;     +Some idiocincracies related to racket especially with apostrophes. 
+;
+; The examples show some sentences from each category of the grammar and include words that can function in more than
+; one way.
+
+
 #lang racket
 
 (require racket/trace)
@@ -98,7 +128,7 @@
   (let ([sub (and (pair? word) (assoc word DB))])
     (if sub (cdr sub) #f)))
 
-(define (translate sent)
+(define (english-to-french sent)
   (let ([ssub (simple-subst sent)])
     (if ssub ssub
         (flatten (append-map
@@ -106,16 +136,16 @@
                     (cond
                       [(simple-subst phrase)]
                       [(not (pair? phrase)) '()]
-                      [(append-map (λ(sub-phrase) (translate sub-phrase)) phrase)]
+                      [(append-map (λ(sub-phrase) (english-to-french sub-phrase)) phrase)]
                       [else '()]
                       ))
                   sent)))))
 
-(define (go sent)
-  (translate (mark-up sent)))
+(define (translate sent)
+  (english-to-french (mark-up sent)))
 
-(go '(the Buffalo buffalo buffalo the big scary dog))
-(go '(the dog runs up the hill))
-(go '(the pretty bird flies))
-(go '(the big scary black dog chases the small pretty white cat))
+(translate '(the Buffalo buffalo buffalo the big scary dog))
+(translate '(the dog runs up the hill))
+(translate '(the pretty bird flies))
+(translate '(the big scary black dog chases the small pretty white cat))
     
