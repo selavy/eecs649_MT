9541a16475b43624900e1361c3ad92f44894fcae
Peter Lesslie <pclesslie@gmail.com>
2014-05-08 08:07:52 -0500

    Fix adjective list

--- machine_translation.rkt
+++ machine_translation.rkt
@@ -3,11 +3,11 @@
 (require racket/trace)
 
 (define articles '(the a))
-(define adjectives '(Buffalo small big scary tall pretty))
+(define adjectives '(black white Buffalo small big scary tall pretty))
 (define conjunctions '(and))
 (define nouns '(buffalo block bird cat dog hill))
 (define prepositions '(over to up))
-(define verbs '(block flies runs buffalo))
+(define verbs '(chases block flies runs buffalo))
 
 (define DB '( 
              ((adjective big) gros)
@@ -15,6 +15,9 @@
              ((adjective scary) effrayant)
              ((adjective pretty) mignon)
              ((adjective tall) grand)
+             ((adjective black) noir)
+             ((adjective white) blanc)
+             ((adjective small) petit)
              ((article the) le)
              ((article a) un)
              ((conjunction and) et)
@@ -25,7 +28,9 @@
              ((noun dog) chien)
              ((noun hill) colline)
              ((preposition up) jusque à)
-             ((verb buffalo) fait peur)  
+             ((preposition to) à)
+             ((verb buffalo) fait peur)
+             ((verb chases) poursuit)
              ((verb block) bloque) 
              ((verb runs) court) 
              ((verb flies) vole)
@@ -39,12 +44,13 @@
 (define (preposition? word) (if (member word prepositions) #t #f))
 
 (define (mark-up sent)
-  (printf "Marking up: ~a.\n" sent)
+  (printf "Translating: ~a.\n" sent)
   (let* ([first-word (car sent)] [rest-of-sentence (cdr sent)] [noun-phrase+verb-phrase (noun-phrase+verb-phrase? rest-of-sentence)])
   (cond
     ((article? first-word) (list (list 'article first-word) 
-                                 (append (filter     (λ(word) (eq? (car word) 'noun)) noun-phrase+verb-phrase)
-                                         (filter-not (λ(word) (eq? (car word) 'noun)) noun-phrase+verb-phrase))))
+                                  (append (filter     (λ(word) (eq? (car word) 'noun)) noun-phrase+verb-phrase)
+                                          (filter-not (λ(word) (eq? (car word) 'noun)) noun-phrase+verb-phrase)) 
+                                  ))
     (noun-phrase+verb-phrase)
     (else #f))))
 
@@ -72,10 +78,10 @@
   (let ([word (and (pair? clause) (car clause))])
     (cond
       ((null? word) #f)
-      ((noun? word) (list 'noun word))
+      ((noun? word) (list (list 'noun word)))
       ((not (pair? clause)) #f)
       ((conjunction? word) (list (adj+obj? (cdr clause))))
-      ((adjective? word) (filter-not (λ(x) (null? x)) (list (adj+obj? (cdr clause)) (list 'adjective word) (if (noun? (cadr clause)) (list 'conjunction 'and) '()))))
+      ((adjective? word) (filter-not (λ(x) (null? x)) (append (adj+obj? (cdr clause)) (list (list 'adjective word)) (list (list 'conjunction 'and)))))
       (else #f)
     )))
 
@@ -84,7 +90,7 @@
     (cond
       ((null? clause) #f)
       ((not (pair? clause)) #f)
-      ((article? word) (list (list 'article word) (adj+obj? (cdr clause))))
+      ((article? word) (list (list 'article word) (drop-right (adj+obj? (cdr clause)) 1)))
       (else #f)
     )))
 
@@ -105,5 +111,11 @@
                       ))
                   sent)))))
 
-(translate (mark-up '(the Buffalo buffalo buffalo the big scary dog)))
+(define (go sent)
+  (translate (mark-up sent)))
+
+(go '(the Buffalo buffalo buffalo the big scary dog))
+(go '(the dog runs up the hill))
+(go '(the pretty bird flies))
+(go '(the big scary black dog chases the small pretty white cat))
     
